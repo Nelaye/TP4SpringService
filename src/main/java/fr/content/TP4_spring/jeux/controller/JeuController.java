66a7f5e7872d7200fe7fca55146cba10e4f214c2
Nelaye <emilien01lambert@gmail.com>
2020-12-09 19:54:56 +0100

    maintenant ça marche

--- src/main/java/fr/content/TP4_spring/jeux/controller/JeuController.java
+++ src/main/java/fr/content/TP4_spring/jeux/controller/JeuController.java
@@ -4,18 +4,17 @@ import fr.content.TP4_spring.jeux.modele.Jeu;
 import fr.content.TP4_spring.jeux.repository.JeuRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
+@EnableDiscoveryClient
 public class JeuController {
 
     @Autowired
     private JeuRepository dao;
-
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
 
     @Value("${me}")
     private String me;
